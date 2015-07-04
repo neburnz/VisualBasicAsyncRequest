453fce32b813daca2b00deffe73fabbf05627658
neburnz <neburnz@hotmail.com>
2015-07-03 19:00:49 -0500

    Refactored first servce and added second service

--- PrimaryService/FirstService.svc.vb
+++ PrimaryService/FirstService.svc.vb
@@ -1,16 +1,16 @@
 ﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service1" in code, svc and config file together.
 ' NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.vb at the Solution Explorer and start debugging.
-Public Class Service1
-    Implements IService1
+Public Class GlobalService
+    Implements IFirstService
 
     Public Sub New()
     End Sub
 
-    Public Function GetData(ByVal value As Integer) As String Implements IService1.GetData
+    Public Function GetData(ByVal value As Integer) As String Implements IFirstService.GetData
         Return String.Format("You entered: {0}", value)
     End Function
 
-    Public Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType Implements IService1.GetDataUsingDataContract
+    Public Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType Implements IFirstService.GetDataUsingDataContract
         If composite Is Nothing Then
             Throw New ArgumentNullException("composite")
         End If
--- SecondaryService/ISecondService.vb
+++ SecondaryService/ISecondService.vb
@@ -1,6 +1,6 @@
 ﻿' NOTE: You can use the "Rename" command on the context menu to change the interface name "IService1" in both code and config file together.
 <ServiceContract()>
-Public Interface IService1
+Public Interface ISecondService
 
     <OperationContract()>
     Function GetData(ByVal value As Integer) As String
